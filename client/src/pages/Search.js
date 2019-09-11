3a069ca4401c2cb7388e83f396298121cfce90e2
steliosk9 <steliosk9@gmail.com>
2019-09-10 19:48:33 -0500

    fix the issue with the react-dome npm package

---
+++ client/src/pages/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import Jumbotron from "../components/Jumbotron";
+import Navbar from "../components/Navbar";
+
+
+
+class Search extends Component {
+
+
+
+render(){
+return(
+< Navbar />
+)
+}
+}
+export default Search;
