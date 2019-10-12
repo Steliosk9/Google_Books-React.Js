522095f80c3fbceb8d4da17ab1a7ec43ec94ede7
steliosk9 <steliosk9@gmail.com>
2019-10-12 15:01:25 -0500

    heroku deplyment

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require("path")
 const PORT = process.env.PORT || 3001;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get('*', (req, res)) =>{
+  res.sendFile(path.join(_dirname, "client", "build", "index.html"))
+  });
 }
 
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/booksapp",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:passwordKk9@ds233268.mlab.com:33268/heroku_53r7b0vq",{
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+ });
+ 
+ 
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
