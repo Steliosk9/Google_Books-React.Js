c5000caceb13fdf0f5462460c9358b51777411c9
steliosk9 <steliosk9@gmail.com>
2019-10-16 23:34:02 -0500

    deployed on Heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/booksapp",
+  process.env.MONGODB_URI || "mongodb://stelios:2109525789Kk9@ds335678.mlab.com:35678/heroku_b13hcljw",
   {
     useCreateIndex: true,
     useNewUrlParser: true
