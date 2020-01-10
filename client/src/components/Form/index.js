393c5dc61211d92de93d71812e4fefecb55c20fa
stylianos kouroupas <steliosk9@gmail.com>
2020-01-09 21:56:53 -0600

    refactored the card component

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="So many books, so little time...."
           name="q"
           onChange={handleInputChange}
           required
